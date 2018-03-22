ddab31a17358a6fe835e58ffd9c134d326254a8f
YorihiroKatsuki <katsuki_y@YK.local>
2018-03-23 01:33:49 +0900

    辞書包括表記

---
+++ dictionary_inner.py
@@ -0,0 +1,19 @@
+# 辞書包括表記
+
+w = ['mon', 'tue', 'wed']
+f = ['coffee', 'milk', 'water']
+
+d = {}
+# zip関数はそれぞれ引数のリストを対応する変数に展開する
+for x,y in zip(w,f):
+    d[x] = y
+
+print(d)
+
+# キー: バリュー for ・・の形
+d = {x: y for x, y in zip(w,f)}
+
+print(d)
+
+# リスト内包表記(参照)
+# r2 = [i for i in t if i % 2 == 1]
